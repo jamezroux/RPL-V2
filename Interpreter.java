0438c3b35479c0a709d2a9988e71700731db84c1
JamezRoux <B1ood6od@gmail.com>
2019-10-08 08:58:50 -0400

    Initial commit

---
+++ Interpreter.java
@@ -0,0 +1,32 @@
+public class Iterpreter() {
+
+  // Language opcodes
+  // Math opcodes
+  final static String OP_ADD = "";
+  final static String OP_SUB = "";
+  final static String OP_MUL = "";
+  final static String OP_DIV = "";
+  final static String OP_NEG = "";
+
+  // Rubiks movement opcodes
+  final static String OP_MOVL = "";
+  final static String OP_MOVLN = "";
+  final static String OP_MOVR = "";
+  final static String OP_MOVRN = "";
+  final static String OP_MOVD = "";
+  final static String OP_MOVDN = "";
+  final static String OP_MOVU = "";
+  final static String OP_MOVUN = "";
+
+  // Pointer opcodes
+  final static String OP_PL = "";
+  final static String OP_PR = "";
+  final static String OP_PF = "";
+  final static String OP_PB = "";
+
+  // Other opcodes
+
+  public static void main(String[] args) {
+
+  }
+}
